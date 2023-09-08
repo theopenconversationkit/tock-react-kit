0c676e0e75cc58f4492ca71b9fa13fa8b6f4b875
Fabilin <fabilin.dev@pm.me>
2023-09-08 17:41:17 +0200

    Add theme configuration to storybook toolbar

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,5 @@
 import type { Preview } from '@storybook/react';
-import ThemeDecorator from './decorators/ThemeDecorator'
+import ThemeDecorator, {palettes, sizings, typographies} from './decorators/ThemeDecorator'
 import TockContextDecorator from "./decorators/TockContextDecorator";
 
 const preview: Preview = {
@@ -15,7 +15,33 @@ const preview: Preview = {
   decorators: [
     ThemeDecorator,
     TockContextDecorator,
-  ]
+  ],
+  globalTypes: {
+    palette: {
+      description: 'Global color palette for components',
+      defaultValue: 'default',
+      toolbar: {
+        title: 'Palette',
+        items: [...Object.keys(palettes)]
+      }
+    },
+    sizing: {
+      description: 'Global sizing for components',
+      defaultValue: 'default',
+      toolbar: {
+        title: 'Sizing',
+        items: [...Object.keys(sizings)]
+      }
+    },
+    typography: {
+      description: 'Global typography for components',
+      defaultValue: 'default',
+      toolbar: {
+        title: 'Typography',
+        items: [...Object.keys(typographies)]
+      }
+    },
+  }
 };
 
 export default preview;
