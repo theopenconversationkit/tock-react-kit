eee59a1d2e8d4de501a9dfe1bda8c0cbdb28ee8a
Fabilin <fabilin.dev@pm.me>
2024-07-15 18:13:52 +0200

    fix #168: add missing locale dependency to sendMessage callback

--- src/useTock.ts
+++ src/useTock.ts
@@ -401,7 +401,7 @@ const useTock: (
         .then(handlePostBotResponse)
         .finally(stopLoading);
     },
-    [],
+    [locale],
   );
 
   const sendReferralParameter: (referralParameter: string) => Promise<void> =
